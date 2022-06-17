5b1e73ba5889b55df4fb52f379b9232c87001d36
LeandroLeeh32 <leandroleeh32@gmail.com>
2022-06-17 17:26:25 -0300

    Download e instação de serviço de forma silenciosa

---
+++ WindowsFormsApp1/ApplicationContext.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using WindowsFormsApp1.Properties;
+using System.Configuration;
+using System.Net;
+using System.ComponentModel;
+using System.Threading;
+using System.ServiceProcess;
+using System.Diagnostics;
+using System.Net.Http;
+using static WindowsFormsApp1.Cryptography;
+
+namespace WindowsFormsApp1
+{
+    public class ApplicationContext : System.Windows.Forms.ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+        string Download = ConfigurationManager.AppSettings["Download"];
+        string ApplicationNameInstaller = ConfigurationManager.AppSettings["ApplicationNameInstaller"];
+        string ServiceName = ConfigurationManager.AppSettings["ServiceName"];
+        string ProxyHost = ConfigurationManager.AppSettings["ProxyHost"];
+        string ProxyPort = ConfigurationManager.AppSettings["ProxyPort"];
+        string ProxyUserName = ConfigurationManager.AppSettings["ProxyUserName"];
+        string ProxyPassword = ConfigurationManager.AppSettings["ProxyPassword"];
+        string ProxyProtocol = ConfigurationManager.AppSettings["ProxyProtocol"];
+        string HasProxy = ConfigurationManager.AppSettings["HasProxy"];
+        string StartAutomatic = ConfigurationManager.AppSettings["StartAutomatic"];
+        Thread _Thread;
+        bool IsAutomaticRunning = false;
+
+        public static byte[] Key = { 124, 222, 121, 82, 172, 21, 185, 111, 228, 182, 72, 132, 233, 123, 80, 12 };
+        public static byte[] IV = { 172, 111, 13, 42, 244, 102, 81, 211 };
+
+        public ApplicationContext()
+        {
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Resources.llight_green,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Exit", Exit),
+                new MenuItem("Download ", Get),
+                new MenuItem("Stop ", Stop),
+                new MenuItem("Start ", Start),
+                new MenuItem("Quiet ", Quiet),
+                new MenuItem("Automatic ", Automatic)
+            }),
+                Visible = true
+            };
+        }
+
+        void Automatic(object sender, EventArgs e)
+        {
+            _Thread = new Thread(new ThreadStart(Sequence));
+            _Thread.Start();
+        }
+
+        void Sequence()
+        {
+            createMenu(true);
+            while (true)
+            {
+                IsAutomaticRunning = true;
+                Get();
+                Stop();
+                Thread.Sleep((1000 * 60) * int.Parse(StartAutomatic));
+            }
+        }
+
+        void Exit_Automatic(object sender, EventArgs e)
+        {
+            if (!IsAutomaticRunning)
+            {
+                _Thread.Abort();
+                createMenu(false);
+            }
+        }
+
+        void Exit(object sender, EventArgs e) => Exit();
+        void Exit()
+        {
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+        void Get(object sender, EventArgs e) => Get();
+        void Get()
+        {
+            WebClient client = new WebClient();
+            string PathResult = Path.Combine(Path.GetTempPath(), ApplicationNameInstaller);
+            client.Proxy = Convert.ToBoolean(HasProxy) ? ResolveWebProxy() : new WebProxy();
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+            client.DownloadFileAsync(new Uri(Download), PathResult);
+        }
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            trayIcon.Icon = Resources.light_red;
+        }
+
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (IsAutomaticRunning) { Quiet(); }
+            trayIcon.Icon = Resources.llight_green;
+        }
+
+        void Stop(object sender, EventArgs e) => Stop();
+        void Stop()
+        {
+            ServiceController service = new ServiceController(ServiceName);
+            if (service.Status == ServiceControllerStatus.Running)
+            {
+                service.Stop();
+                service.WaitForStatus(ServiceControllerStatus.Stopped);
+            }
+        }
+
+        void Start(object sender, EventArgs e) => Start();
+        void Start()
+        {
+            ServiceController service = new ServiceController(ServiceName);
+            if (service.Status == ServiceControllerStatus.Stopped)
+            {
+                service.Start();
+                service.WaitForStatus(ServiceControllerStatus.Running);
+            }
+        }
+
+        void Quiet(object sender, EventArgs e) => Quiet();
+        void Quiet()
+        {
+            trayIcon.Icon = Resources.light_red;
+            string PathResult = Path.Combine(Path.GetTempPath(), ApplicationNameInstaller);
+           Process process = new Process();
+            {
+                process.StartInfo.FileName = PathResult;
+                process.StartInfo.Arguments = " /SILENT | /VERYSILENT [/SUPPRESSMSGBOXES]";
+                process.EnableRaisingEvents = true;
+                process.Exited += process_Exited;
+                process.Start();
+                process.WaitForExit();
+            }
+
+        }
+
+        private void process_Exited(object sender, EventArgs e)
+        {
+            var myProcess = (Process)sender;
+            if (myProcess.ExitCode == 0)
+            {
+                if (IsAutomaticRunning) { Start(); };
+                IsAutomaticRunning = false;
+                trayIcon.Icon = Resources.llight_green;
+            }
+        }
+
+        void createMenu(bool IsAutomatic)
+        {
+            if (IsAutomatic)
+            {
+                trayIcon.ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Stop Automatic", Exit_Automatic) });
+            }
+            else
+            {
+                trayIcon.Icon = Resources.llight_green;
+                trayIcon.ContextMenu = new ContextMenu(new MenuItem[] {
+                    new MenuItem("Exit", Exit),
+                    new MenuItem("Download ", Get),
+                    new MenuItem("Stop ", Stop),
+                    new MenuItem("Start ", Start),
+                    new MenuItem("Quiet ", Quiet),
+                    new MenuItem("Automatic ", Automatic)});
+            }
+        }
+
+        private WebProxy ResolveWebProxy()
+        {
+
+            Cryptography.Key = Key; Cryptography.IV = IV;
+
+            return new WebProxy
+            {
+            Address = new Uri($"{Cryptography.DecryptString(ProxyProtocol)}://{Cryptography.DecryptString(ProxyHost)}:{Cryptography.DecryptString(ProxyPort)}"),
+                BypassProxyOnLocal = false,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(
+                   userName: Cryptography.DecryptString(ProxyUserName),
+                   password: Cryptography.DecryptString(ProxyPassword))
+            };
+    }
+
+}
+}
---
+++ WindowsFormsApp1/Cryptography.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Cryptography
+    {
+        private static TripleDESCryptoServiceProvider clientDESCryptoServiceProvider;
+
+        public static byte[] Key
+        {
+            get
+            {
+                return Cryptography.clientDESCryptoServiceProvider.Key;
+            }
+            set
+            {
+                Cryptography.clientDESCryptoServiceProvider.Key = value;
+            }
+        }
+
+        public static byte[] IV
+        {
+            get
+            {
+                return Cryptography.clientDESCryptoServiceProvider.IV;
+            }
+            set
+            {
+                Cryptography.clientDESCryptoServiceProvider.IV = value;
+            }
+        }
+
+        [DebuggerNonUserCode]
+        public Cryptography()
+        {
+
+        }
+
+        static Cryptography()
+        {
+            Cryptography.clientDESCryptoServiceProvider = new TripleDESCryptoServiceProvider();
+        }
+
+        public static string EncryptString(string AString)
+        {
+            MemoryStream memoryStream = null;
+            StreamWriter streamWriter = null;
+            CryptoStream cryptoStream = null;
+            checked
+            {
+                string result;
+                ICryptoTransform transform = Cryptography.clientDESCryptoServiceProvider.CreateEncryptor();
+                try
+                {
+                    memoryStream = new MemoryStream();
+                    try
+                    {
+                        cryptoStream = new CryptoStream(memoryStream, transform, CryptoStreamMode.Write);
+                        try
+                        {
+                            streamWriter = new StreamWriter(cryptoStream);
+                            streamWriter.Write(AString);
+                            streamWriter.Flush();
+                            cryptoStream.FlushFinalBlock();
+                            memoryStream.Position = 0L;
+                            byte[] array = new byte[(int)memoryStream.Length + 1];
+                            memoryStream.Read(array, 0, (int)memoryStream.Length);
+                            result = Convert.ToBase64String(array);
+                        }
+                        finally
+                        {
+                            streamWriter.Close();
+                        }
+                    }
+                    finally
+                    {
+                        cryptoStream.Close();
+                    }
+                }
+                finally
+                {
+                    memoryStream.Close();
+                }
+                return result;
+            }
+        }
+
+        public static string DecryptString(string AString)
+        {
+            string result = string.Empty;
+            MemoryStream memoryStream = null;
+            CryptoStream cryptoStream = null;
+            checked
+            {
+                byte[] array = Convert.FromBase64String(AString);
+                try
+                {
+                    memoryStream = new MemoryStream();
+                    try
+                    {
+                        ICryptoTransform transform = Cryptography.clientDESCryptoServiceProvider.CreateDecryptor();
+                        cryptoStream = new CryptoStream(memoryStream, transform, CryptoStreamMode.Write);
+                        cryptoStream.Write(array, 0, array.Length - 1);
+                        cryptoStream.FlushFinalBlock();
+                        memoryStream.Position = 0L;
+                        int num = (int)memoryStream.Length;
+                        array = new byte[num - 1 + 1];
+                        memoryStream.Read(array, 0, num);
+                        int arg_8D_0 = 0;
+                        int num2 = num - 1;
+                        int num3 = arg_8D_0;
+                        string text = string.Empty;
+                        while (true)
+                        {
+                            int arg_B7_0 = num3;
+                            int num4 = num2;
+                            if (arg_B7_0 > num4)
+                            {
+                                break;
+                            }
+                            text += Convert.ToString((Char)array[num3]);
+                            num3++;
+                        }
+                        result = text;
+                    }
+                    finally
+                    {
+                        cryptoStream.Close();
+                    }
+                }
+                finally
+                {
+                    memoryStream.Close();
+                }
+                return result;
+            }
+        }
+    }
+}
